a6ed8c851e5a18c4598839bcf895959314785af5
Digantar kumar singh <33173401+digantar007@users.noreply.github.com>
2025-02-24 02:27:41 +0530

    Update page.tsx

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -2,7 +2,6 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
-import { useState } from 'react';
 import { FaFacebook, FaFootballBall, FaInstagram, FaTwitter } from 'react-icons/fa';
 
 const cricketImages = ['/cricket.jpg', '/cricket.jpg', '/cricket.jpg', '/cricket.jpg'];
